7660006ad903561541688d5089b3a95166b1373e
huydang208 <huydang191@gmail.com>
2021-11-03 03:31:57 +0700

    second

---
+++ src/app/feature-modules/commons/shared-modules/shared-modules.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModulesComponent } from './shared-modules.component';
+
+
+
+@NgModule({
+  declarations: [
+    SharedModulesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModulesModule { }
